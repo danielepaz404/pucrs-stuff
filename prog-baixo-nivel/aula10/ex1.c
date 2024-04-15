9deac4194784b580fb7e17b1e284e865c9b4e377
Daniele Paz <79173163+danielepaz404@users.noreply.github.com>
2024-04-15 02:05:13 +0000

    :sparkles: Add ex1

---
+++ prog-baixo-nivel/aula10/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main() {
+    int segundos = 0;
+    int minutos = 0;
+    int horas = 0;
+
+    printf("Informe o tempo em segundos\n");
+    scanf("%d", &segundos);
+
+    if(segundos >= 60) {
+        minutos = segundos / 60;
+        segundos = segundos % 60;
+
+        if(minutos >= 60) {
+            horas = minutos / 60;
+            minutos = minutos % 60; 
+        }
+    }
+
+    printf("Horas: %d, Minutos: %d, Segundos: %d\n", horas, minutos, segundos);
+}
